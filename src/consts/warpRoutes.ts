98c81ef3e3731333c5dcc5c33af45ee004052d88
Eda Akturk <edaakturk@Edas-MacBook-Pro.local>
2024-08-21 11:15:53 +0200

    warp routes

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -5,25 +5,33 @@ export const warpRouteConfigs: WarpCoreConfig = {
     {
       // Base Sepolia (origin chain)
       chainName: 'basesepolia',
-      standard: TokenStandard.EvmHypNative,  // Since it's ETH, we use EvmHypNative
+      standard: TokenStandard.EvmHypNative, // Since it's ETH, we use EvmHypNative
       decimals: 18,
       symbol: 'ETH',
       name: 'Ether',
-      addressOrDenom: '0x9b7f03Bcd982FB2e32332148E234B15bEC85884e',  // Your Warp Route address on Base Sepolia
-      logoURI: '/logos/weth.png',  // Add the logo in this path
-      connections: [{ token: 'ethereum|sepolia|0x3A4E5844F16e4E3627A29B2fE1243308711BB167' }]
+      addressOrDenom: '0x9b7f03Bcd982FB2e32332148E234B15bEC85884e', // Your Warp Route address on Base Sepolia
+      logoURI: '/logos/weth.png', // Add the logo in this path
+      connections: [
+        {
+          token: 'ethereum|sepolia|0x3A4E5844F16e4E3627A29B2fE1243308711BB167',
+        },
+      ],
     },
     {
       // Ethereum Sepolia (destination chain)
       chainName: 'sepolia',
-      standard: TokenStandard.EvmHypSynthetic,  // Synthetic version of ETH on Sepolia
+      standard: TokenStandard.EvmHypSynthetic, // Synthetic version of ETH on Sepolia
       decimals: 18,
       symbol: 'ETH',
       name: 'Ether',
-      addressOrDenom: '0x3A4E5844F16e4E3627A29B2fE1243308711BB167',  // Your Warp Route address on Ethereum Sepolia
-      logoURI: '/logos/weth.png',  // Same ETH logo
-      connections: [{ token: 'ethereum|basesepolia|0x9b7f03Bcd982FB2e32332148E234B15bEC85884e' }]
-    }
+      addressOrDenom: '0x3A4E5844F16e4E3627A29B2fE1243308711BB167', // Your Warp Route address on Ethereum Sepolia
+      logoURI: '/logos/weth.png', // Same ETH logo
+      connections: [
+        {
+          token: 'ethereum|basesepolia|0x9b7f03Bcd982FB2e32332148E234B15bEC85884e',
+        },
+      ],
+    },
   ],
   options: {},
 };
